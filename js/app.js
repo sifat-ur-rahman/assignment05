bd012eff4aabaeb25821957c16a2109f609f4d9c
sifat-ur-rahman <md.sifat.ur.rahman2702@gmail.com>
2022-08-22 14:08:12 +0600

    added-total

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function displayName(playerName) {
         const name = cartArray[i]
         const tr = document.createElement('tr')
         tr.innerHTML = `
-        <th>${i + 1}. </th>
+        <th>${i + 1}.</th>
         <td> ${name}</td<
         `
         oderList.appendChild(tr)
@@ -22,3 +22,36 @@ function addToCart(element) {
     document.getElementById('player-selected').innerText = cartArray.length
     displayName(cartArray)
 }
+function textToNumber(elementId) {
+    const textFild = document.getElementById(elementId)
+    const textString = textFild.innerText
+    const textNumber = parseFloat(textString)
+    return textNumber
+}
+function input(inputElement) {
+    const inputElemint = document.getElementById(inputElement);
+    const inputString = inputElemint.value;
+    const inputNumber = parseFloat(inputString)
+    return inputNumber
+}
+function setTextElementById(elementId, newElement) {
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newElement
+
+}
+document.getElementById('Calculate').addEventListener('click', function () {
+
+    const nambersOfPalyer = textToNumber('player-selected')
+    const perPlayerAmount = input('per-player')
+    const playerExpenses = nambersOfPalyer * perPlayerAmount
+
+    setTextElementById('player-expenses', playerExpenses)
+})
+document.getElementById('calculate-total').addEventListener('click', function () {
+    const playerExpenses = textToNumber('player-expenses')
+    const managerAmount = input('manager')
+    const coachAmount = input('coach')
+
+    const total = playerExpenses + managerAmount + coachAmount
+    setTextElementById('total', total)
+})
